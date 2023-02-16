42a44af9c03ead221377ecae5375dc38f8c8c349
WSQ.coder <WSQ.coder@gmail.com>
2023-02-16 16:05:26 +0300

    docs: first test is OK

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ task from https://github.com/rolling-scopes-school/brackets
 
 ## Task
 
-Implement function `check(str, bracketsConfig)`, that for given brackets sequence will return `true` if it is correct and `false` otherwise
+	Implement function `check(str, bracketsConfig)`, that for given 				brackets sequence will return `true` if it is correct and `false` otherwise
 
 In the second param there is `bracketsConfig` - the array of pairs open-closed brackets. Each subarray includes only 2 elements - opening and closing bracket
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  console.log(`\n\nstr =`, str);
+  let pairs = {},
+    openBracketsStack = [],
+    key, a, b, waitBracket;
+
+  for (key in bracketsConfig) {
+    [a, b] = bracketsConfig[key];
+    pairs[a] = b;
+  }
+  console.log("pairs =", pairs);
+
+  for (let a of str) {
+    if (a in pairs) {
+      // this is open bracker - save to stack
+      openBracketsStack.push(a);
+      continue;
+    }
+
+    // this is closing brackets. Test for pairs
+		waitBracket = pairs[openBracketsStack.pop()]
+    if (a !== waitBracket) {
+      console.log(`An erroneous closing bracket "${a}". I wait "${waitBracket}"`);
+
+      return false;
+    }
+  }
+
+  if (openBracketsStack.length !== 0) {
+		console.log(`There are not closed brackets:`, openBracketsStack)
+		
+    return false;
+  } else {
+    return true;
+  }
+};
