9b50bd912bf7e20ecebfbfe4b234e150823c9f85
WSQ.coder <WSQ.coder@gmail.com>
2023-02-16 21:41:53 +0300

    docs: all tests are ready

--- src/index.js
+++ src/index.js
@@ -1,36 +1,62 @@
 module.exports = function check(str, bracketsConfig) {
-  console.log(`\n\nstr =`, str);
-  let pairs = {},
-    openBracketsStack = [],
-    key, a, b, waitBracket;
+	let openBracketsStack = [],
+	pairs = {};
+	
+	const debug = false;
+  if (debug) console.log(`\n\nstr = "${str}"`);
+  
+	pairs = convertToPairs(bracketsConfig);
+  if (debug) console.log("pairs =", pairs);
 
-  for (key in bracketsConfig) {
-    [a, b] = bracketsConfig[key];
-    pairs[a] = b;
-  }
-  console.log("pairs =", pairs);
+  for (let char of str) {
+    if (debug) console.log(`Processing the symbol "${char}"`);
 
-  for (let a of str) {
-    if (a in pairs) {
-      // this is open bracker - save to stack
-      openBracketsStack.push(a);
-      continue;
+    if (canCloseBracket(char, openBracketsStack, pairs)) {
+      if (debug) console.log("   this is closing bracket");
+      openBracketsStack.pop(); // use closing bracket
+      //
+    } else {
+      if (char in pairs) {
+        if (debug) console.log(`   save opening bracket "${char}"`);
+        openBracketsStack.push(char); //save new open bracket
+      } else {
+        if (debug) console.log(`   this is not open bracket "${char}"`);
+        return false; //this is not open bracket
+      }
     }
-
-    // this is closing brackets. Test for pairs
-		waitBracket = pairs[openBracketsStack.pop()]
-    if (a !== waitBracket) {
-      console.log(`An erroneous closing bracket "${a}". I wait "${waitBracket}"`);
-
-      return false;
-    }
   }
 
-  if (openBracketsStack.length !== 0) {
-		console.log(`There are not closed brackets:`, openBracketsStack)
-		
+  // str fully processed
+  if (debug) console.log("str fully processed. Now stack =", openBracketsStack);
+  if (openBracketsStack.length > 0) {
     return false;
   } else {
     return true;
   }
 };
+
+//
+// ADDITIONAL FUNCTIONS
+//
+function convertToPairs(config) {
+  let resultPairs = {},
+    a,
+    b;
+  for (let key in config) {
+    [a, b] = config[key];
+    resultPairs[a] = b;
+  }
+  return resultPairs;
+}
+
+function canCloseBracket(nextBracket, openBracketsStack, pairs) {
+  if (openBracketsStack.length == 0) return false;
+
+  let lastOpenBracket = openBracketsStack.slice(-1)[0];
+  let waitCloseBracket = pairs[lastOpenBracket];
+  if (nextBracket === waitCloseBracket) {
+    return true;
+  } else {
+    return false;
+  }
+}
